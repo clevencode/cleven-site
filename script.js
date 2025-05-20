2dd9f5bfc3791ff86e114e63d0c1424dca5f370d
Clevenider Petit <clevypetit@gmail.com>
2025-05-19 23:03:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -37,6 +37,19 @@ if (navbar) {
     lastScrollTop = Math.max(0, currentScroll);
   });
 }
+// -------------------------------
+// Tabs: mostra o conteúdo correspondente
+// -------------------------------
+  const tabs = document.querySelectorAll('.tab');
+  const contents = document.querySelectorAll('.tab-content');
 
+  tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+      tabs.forEach(t => t.classList.remove('active'));
+      contents.forEach(c => c.classList.remove('active'));
 
+      tab.classList.add('active');
+      document.getElementById(tab.dataset.tab).classList.add('active');
+    });
+  });
 
